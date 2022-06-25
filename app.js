a5dacda02ca156f225f233802467f9f389c0d75b
Sean K <Trueflash42@outlook.com>
2022-06-25 13:18:04 -0400

    commit on main

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.get("/help", function(req, res){
     res.render("help", {}); 
 });
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server started on port 3000.");
 });
