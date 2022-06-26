7c40d4031103cdf9c598ba59ad36db5a028742ff
Sean K <Trueflash42@outlook.com>
2022-06-25 22:37:32 -0400

    commit on main

--- app.js
+++ app.js
@@ -22,10 +22,6 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.render("home", {});
-});
-
-app.get("/about", function (req, res) {
   res.render("about", {});
 });
 
